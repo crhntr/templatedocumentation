54430196745b3ebfa8b933a932fb93b791f4d23c
Chrstopher Hunter <8398225+crhntr@users.noreply.github.com>
2024-07-15 17:18:42 -0700

    add tests

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/crhntr/templatedocumentation
 
 go 1.22
+
+require (
+	github.com/crhntr/dom v0.1.0-dev.3
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ handler_test.go
@@ -0,0 +1,61 @@
+package templatedocumentation_test
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/crhntr/dom/domtest"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/crhntr/templatedocumentation"
+)
+
+func TestHandler(t *testing.T) {
+	functions := template.FuncMap{
+		"now":      time.Now,
+		"markdown": func(s string) template.HTML { return "" },
+	}
+	ts := template.Must(template.New("testdata").Funcs(functions).ParseFiles(filepath.Join("testdata", "templates.gohtml")))
+
+	h := templatedocumentation.Handler(ts, functions)
+
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/", nil)
+	h.ServeHTTP(rec, req)
+
+	res := rec.Result()
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+
+	document := domtest.Response(t, res)
+
+	if index := document.QuerySelector(`#index`); assert.NotNil(t, index) {
+		links := index.QuerySelectorAll(`a[href*="#"]`)
+		for i := 0; i < links.Length(); i++ {
+			link := links.Item(i)
+			id := strings.TrimPrefix(link.GetAttribute("href"), "#")
+			div := document.QuerySelector(fmt.Sprintf("[id=%q]", id))
+			if assert.NotNil(t, div, "missing element for %q", id) {
+				assert.True(t, div.Matches(`div`), "is a div")
+				assert.True(t, div.Matches(`.define-template`), "has class define-template")
+				assert.NotNil(t, div.QuerySelector(`pre`), "has source")
+			}
+		}
+	}
+
+	assert.Nil(t, document.QuerySelector(`#index a[href="#template--empty"]`))
+	assert.Nil(t, document.QuerySelector(`[id="template--empty"].define-template`))
+
+	if footerSource := document.QuerySelector(`[id="template--footer"]>pre`); assert.NotNil(t, footerSource) {
+		assert.NotNil(t, footerSource.QuerySelector(`a[href="#template--nav"]`))
+	}
+
+	if t.Failed() && testing.Verbose() {
+		t.Log(document)
+	}
+}
